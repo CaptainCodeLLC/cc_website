314ec844381f337ff73d1ad49abe405c83bd3925
Colombianito66 <colombianito66@gmail.com>
2018-07-10 15:17:45 -0400

    connect form to db

--- client/src/components/pages/contact/index.js
+++ client/src/components/pages/contact/index.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react';
 import {Form, FormGroup, Label, Input, Container, Row, Col,Button} from 'reactstrap';
+import axios from 'axios';
+
+
 export default class Contact extends Component {
+    state = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+        comments: ''
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+
+        axios.post('/users', this.state)
+            .then(res => console.log('Axios Response', res))
+            .catch(err => console.log(err))
+    }
+
   render() {
     return (
       <div>
@@ -10,32 +34,67 @@ export default class Contact extends Component {
               <hr/>
                 <Col id = "contact-page-container" xs="12" md="6" >
                     <Form>
-                    <FormGroup>
+                        <FormGroup>
                             <Label for="name">What's your name?</Label>
-                            <Row xs= "12">
-                                <Col>
-                                    <Input type="name" name="first-name" id="example_name" placeholder="First Name" />
-                                </Col>
-                                <Col>
-                                    <Input type="name" name="last-name" id="example_name" placeholder="Last Name" />
-                                </Col>
-                            </Row>
+                                <Row xs= "12">
+                                    <Col>
+                                        <Input 
+                                            type="name" 
+                                            name="firstName" 
+                                            id="example_name" 
+                                            placeholder="First Name"
+                                            value={this.state.firstName}
+                                            onChange={e => this.handleChange(e)}
+                                        />
+                                    </Col>
+                                    <Col>
+                                        <Input 
+                                            type="name" 
+                                            name="lastName" 
+                                            id="example_name" 
+                                            placeholder="Last Name"
+                                            value={this.state.lastName}
+                                            onChange={e => this.handleChange(e)}
+                                        />
+                                    </Col>
+                                </Row>
                         </FormGroup>
                         <FormGroup>
                             <Label for="exampleEmail">Where can we reach you?</Label>
-                            <Input type="email" name="email" id="example_name" placeholder="name@email.com*" />
+                                <Input 
+                                    type="email" 
+                                    name="email" 
+                                    id="example_name" 
+                                    placeholder="name@email.com*" 
+                                    value={this.state.email}
+                                    onChange={e => this.handleChange(e)}
+                                />
                         </FormGroup> 
                         <FormGroup>
-                             <Input type="email" name="email" id="example_name" placeholder="+1 (555) 555-5555" />
+                             <Input 
+                                type="tel" 
+                                name="phone" 
+                                id="example_name" 
+                                placeholder="+1 (555) 555-5555" 
+                                value={this.state.phone}
+                                onChange={e => this.handleChange(e)}
+                            />
                         </FormGroup>
                         <FormGroup>
                             <Label for="exampleText">Tell us what's happening</Label>
-                            <Input type="textarea" placeholder = "What's on your mind?*" name="text" id="exampleText" />
+                                <Input 
+                                    type="textarea" 
+                                    placeholder="What's on your mind?*" 
+                                    name="comments" 
+                                    id="exampleText" 
+                                    value={this.state.comments}
+                                    onChange={e => this.handleChange(e)}
+                                />
                         </FormGroup>
                     </Form>
                 </Col>
               
-                <Button color = "success">Submit</Button> 
+                <Button color="success" onClick={e => this.handleSubmit(e)}>Submit</Button> 
                 <br/>
                 <br/>
                 <p3>* is required</p3>
