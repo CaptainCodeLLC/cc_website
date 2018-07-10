b1f6b83b137247e51fb7ea302a72b2debf26b250
Colombianito66 <colombianito66@gmail.com>
2018-07-09 23:59:37 -0400

    add user get/post routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
+
+module.exports = (app) => {
+
+  app.get("/users", (req, res) => {
+    User.find({}, (err, users) => {
+      console.log("userRoutes.js ReturnUsers: ", users);
+      if (err) return res.send(err);
+      res.send(users);
+    });
+  });
+
+  app.post("/users", (req, res) => {
+    let newUser = new User(req.body);
+
+    newUser.save()
+      .then(user => {
+        res.send("user saved to the database");
+      })
+      .catch(err => {
+        res.status(400).send("unable to save to the database");
+      });
+  });
+}
